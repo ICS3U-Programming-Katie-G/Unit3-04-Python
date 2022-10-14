43eb1120bc873537831038ec194015ce1cedf268
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-10-14 18:13:17 +0000

    integers are funny. :) here is my code for integer checking program :)

---
+++ integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Created by: Katie G
+# Created on: October 13th, 2022
+# This program gets an integer from
+# the user and checks to see if it is
+# positive, negative, or zero using
+# an if.. else if.. else statement.
+
+# this function will get the integer,
+# then check to see if it is positive,
+# negative, or zero.
+def main():
+    # getting the integer from the user
+    user_int = int(input("Hello :) Integer please :) "))
+
+    # if.. else if.. else statement to check
+    # if user int is positive, negative or zero.
+    if (user_int >= 1):
+        print("Oh. Your integer ({}) is positive.".format(user_int))
+    elif (user_int <= -1):
+        print("Oh. Your integer ({}) is negative.".format(user_int))
+    else:
+        print("Oh. Your integer ({}) is zero.".format(user_int))
+
+
+if __name__ == "__main__":
+    main()
